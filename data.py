b40b4b8b58f35fcbaa34f73ce26c7c8dd70c80eb
zhpmatrix <zhpmatrix@gmail.com>
2018-08-16 18:33:22 +0800

    add

---
+++ data.py
@@ -0,0 +1,116 @@
+import re
+import pickle
+import pandas as pd
+import numpy as np
+from itertools import chain
+from os import makedirs
+from os.path import exists, join
+
+def get_dict(data):
+    all_datas_set = pd.Series(list(chain(*data))).value_counts().index
+    all_datas_ids = range(1, len(all_datas_set) + 1)
+    # Dict to transform
+    data2id = pd.Series(all_datas_ids, index=all_datas_set)
+    id2data = pd.Series(all_datas_set, index=all_datas_ids)
+    return data2id, id2data
+
+def transform(data, dict_, max_length):
+    ids = list(dict_[data])
+    if len(ids) >= max_length:
+        ids = ids[:max_length]
+    ids.extend([0] * (max_length - len(ids)))
+    return ids
+
+def save_data(datas, path, filename):
+    path = 'data/'
+    if not exists(path):
+        makedirs(path)
+    print('Starting pickle to file...')
+    with open(join(path,filename), 'wb') as f:
+        for data in datas:
+            pickle.dump(data, f)
+    print('Pickle finished')
+
+def get_dicts(data_path, save_path, filename):
+    
+    text = open(data_path, encoding='utf-8').readlines()
+
+    # To numpy array
+    char, pos, tag  = [], [], []
+    print('Start creating chars, pos and tags...')
+    for i,sentence in enumerate(text):
+        groups = re.findall('(.)&(.-.)&(.-.|.)', sentence)
+        arrays = np.asarray(groups)
+        char.append(arrays[:, 0])
+        pos.append(arrays[:, 1])
+        tag.append(arrays[:, 2])
+
+    # Char, pos, tag set and ids
+    char2id, id2char = get_dict(char)
+    pos2id, id2pos = get_dict(pos)
+    tags_set = ['X', 'O','B-R', 'I-R', 'B-M', 'I-M', 'B-S', 'I-S', 'B-W', 'I-W']
+    #tags_ids = range(1, len(tags_set)+1)
+    tags_ids = range(len(tags_set))
+    # Dict to transform
+    tag2id = pd.Series(tags_ids, index=tags_set)
+    id2tag = pd.Series(tags_set, index=tag2id)
+    
+    datas = [char2id, id2char, pos2id,  id2pos, tag2id, id2tag]
+
+    save_data(datas, save_path, filename)
+
+def read_data(data_path, dicts):
+    char2id, pos2id, tag2id = dicts
+    
+    text = open(data_path, encoding='utf-8').readlines()    
+    # To numpy array
+    char, pos, tag  = [], [], []
+    print('Start creating chars, pos and tags...')
+    for i,sentence in enumerate(text):
+        groups = re.findall('(.)&(.-.)&(.-.|.)', sentence)
+        arrays = np.asarray(groups)
+        char.append(arrays[:, 0])
+        pos.append(arrays[:, 1])
+        tag.append(arrays[:, 2])
+    max_length = 200
+    print('Starting transform...')
+
+    data_char = list(map(lambda x: transform(x, char2id, max_length), char))
+    data_pos = list(map(lambda x: transform(x, pos2id, max_length), pos))
+    data_tag = list(map(lambda x: transform(x, tag2id, max_length), tag))
+    
+    data_char = np.asarray(data_char)
+    data_pos = np.asarray(data_pos)
+    data_tag = np.asarray(data_tag)
+    
+    return data_char, data_pos, data_tag
+
+def save_datas(dict_path, train_data_path, dev_data_path, test_data_path):
+    
+    with open(dict_path, 'rb') as f:
+        char2id = pickle.load(f)
+        id2char = pickle.load(f)
+        pos2id  = pickle.load(f)
+        id2pos  = pickle.load(f)
+        tag2id  = pickle.load(f)
+        id2tag  = pickle.load(f)
+    dicts = [char2id, pos2id, tag2id]
+
+    train_data_char, train_data_pos, train_data_tag = read_data(train_data_path, dicts) 
+    dev_data_char, dev_data_pos, dev_data_tag = read_data(dev_data_path, dicts) 
+    test_data_char, test_data_pos, test_data_tag = read_data(test_data_path, dicts) 
+
+    
+    datas = [   train_data_char,    dev_data_char,  test_data_char,
+                train_data_pos,     dev_data_pos,   test_data_pos,
+                train_data_tag,     dev_data_tag,   test_data_tag
+            ]
+
+    save_data(datas, 'data/', 'data.pkl')
+
+if __name__ == '__main__':
+    get_dicts('data/input/merge_seq.txt','data/','dict.pkl')
+    data_dir = 'data/input/'
+    save_datas('data/dict.pkl', data_dir+'train_seq.txt',
+                                data_dir+'dev_seq.txt',
+                                data_dir+'test_seq.txt')
